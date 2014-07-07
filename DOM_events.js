cd004e4105db223d644416f18007e6f6b25526b3
Serhey Shmyg <serhey.shmyg.all@gmail.com>
2014-07-07 10:27:50 +0300

    version 1.0.0

---
+++ DOM_events.js
@@ -0,0 +1,218 @@
+(function(window) {
+	var version = '1.0.0',
+		separator = '.',
+		eventsList = ['blur', 'focus', 'focusin', 'focusout', 'load', 'resize', 'scroll', 'unload', 'click', 'dblclick', 'mousedown', 'mouseup', 'mousemove', 'mouseover', 'mouseout', 'mouseenter', 'mouseleave', 'change', 'select', 'submit', 'keydown', 'keypress', 'keyup', 'error', 'contextmenu'],
+		isDomEvent = function(eventName) {
+			return eventsList.some(function(item) {
+				return (eventName === item) && 'on' + eventName in window;
+			});
+		},
+		_isElement = function(obj) {
+			try {
+				return obj instanceof HTMLElement;
+			}
+			catch(e){
+				return (typeof obj==='object') && (obj.nodeType === 1) && (typeof obj.style === 'object') && (typeof obj.ownerDocument === 'object');
+			}
+		},
+		removeListener = function(el, eventName, arr, handler) {
+			arr.forEach(function(item, i) {
+				if (handler && handler !== item.userHandler) return true;
+
+				if ( isDomEvent(eventName) ) {
+					el.removeEventListener(eventName, item, false);
+					return true;
+				}
+
+				arr.splice(i, 1);
+			});
+		},
+		eventsIterator = function(el, types, callback, callbackEnd) {
+			var eventName, eventId, result;
+
+			if (!types) {
+				var obj2 = el.events,
+					key2;
+				for (key2 in obj2) {
+					if (!obj2.hasOwnProperty(key2)) continue;
+					
+					var obj3 = obj2[key2],
+						key3;
+					for (key3 in obj3) {
+						if (!obj3.hasOwnProperty(key3)) continue;
+						if (typeof callback === 'function') callback(key2, eventId, obj3[key3]);
+					}
+				}
+
+				if (typeof callbackEnd === 'function') callbackEnd(key2, eventId, 3);
+				return true;
+			}
+
+			types = types.split(' ');
+
+			types.forEach(function(item) {
+				item = item.split(separator);
+				eventName = item[0];
+				eventId = item[1];
+
+				if (el.events[eventName] && el.events[eventName][eventId]) {
+					if (typeof callback === 'function') callback(eventName, eventId, el.events[eventName][eventId]);
+					if (typeof callbackEnd === 'function') callbackEnd(eventName, eventId, 1);
+				}
+
+				if (eventName && !eventId) {
+					var obj = el.events[eventName],
+						key;
+					for (key in obj) {
+						if (!obj.hasOwnProperty(key)) continue;
+						if (typeof callback === 'function') callback(eventName, eventId, obj[key]);
+					}
+
+					if (typeof callbackEnd === 'function') callbackEnd(eventName, eventId, 2);
+				}
+
+				if (!el.events[eventName] && eventId) {
+					var obj4 = el.events,
+						key4;
+					for (key4 in obj4) {
+						if (!obj4.hasOwnProperty(key4) || !obj4[key4][eventId]) continue;
+						if (typeof callback === 'function') callback(key4, eventId, obj4[key4][eventId]);
+						if (typeof callbackEnd === 'function') callbackEnd(key4, eventId, 4, obj4[key4]);
+					}
+				}
+			});
+		},
+		Edom = function(element) {
+			this.element = element;
+		};
+
+	Edom.version = version;
+
+	//[element], types, [data], handler
+	Edom.prototype.on = function() {
+		var length = arguments.length,
+			el = _isElement(arguments[0]) ? arguments[0] : this.element,
+			types, data, handler, eventName, eventId, handlerWraper;
+
+		if (!el) return this;
+		if (!el.events) el.events = {};
+		if (!this.element && el) this.element = el;
+
+		if (length === 2) {
+			types = arguments[0];
+			handler = arguments[1];
+		}
+
+		if (length === 3) {
+			types = (typeof arguments[1] === 'string') ? arguments[1] : arguments[0];
+			data = (typeof arguments[1] !== 'string') ? arguments[1] : undefined;
+			handler = (typeof arguments[2] === 'function') ? arguments[2] : arguments[3];
+		}
+
+		if (length === 4) {
+			el = arguments[0];
+			types = arguments[1];
+			data = arguments[2];
+			handler = arguments[3];
+		}
+
+		handlerWraper = function(e) {
+			if (arguments.length === 1 && typeof arguments === 'object' && data) arguments[0].data = data;
+			handler.apply(this, arguments);
+		};
+
+		handlerWraper.userHandler = handler;
+
+		types = types.split(' ');
+
+		types.forEach(function(item) {
+			item = item.split(separator);
+			eventName = item[0];
+			eventId = item[1];
+
+			if (!el.events[eventName]) el.events[eventName] = {};
+			
+			if (!eventId) {
+				if (!el.events[eventName].all) el.events[eventName].all = [];
+				el.events[eventName].all.push(handlerWraper);
+			} else {
+				if (!el.events[eventName][eventId]) el.events[eventName][eventId] = [];
+				el.events[eventName][eventId].push(handlerWraper)
+			}
+
+			if ( isDomEvent(eventName) ) el.addEventListener(eventName, handlerWraper, false);
+		});
+
+		return this;
+	};
+
+	//[element], types, [handler]
+	Edom.prototype.off = function() {
+		var length = arguments.length,
+			el, types, eventName, handler, eventId;
+
+		if (!length) {
+			el = this.element;
+		}
+
+		if (length === 1) {
+			el = this.element;
+			types = arguments[0];
+		}
+
+		if (length === 2 && typeof arguments[1] === 'string') {
+			el = arguments[0];
+			types = arguments[1];
+		}
+
+		if (length === 2 && typeof arguments[1] === 'function') {
+			el = this.element;
+			types = arguments[0];
+			handler = arguments[1];
+		}
+
+		if (length === 3) {
+			el = arguments[0];
+			types = arguments[1];
+			handler = arguments[2];
+		}
+
+		eventsIterator(el, types, 
+			function(name, id, arr) {
+				removeListener(el, name, arr, handler);
+			}, function(name, id, ifCounter, eventObj) {
+				switch (ifCounter){
+					case 1: delete el.events[name][id];
+					break;
+					case 2: if (!handler) delete el.events[name];
+					break;
+					case 3: delete el.events;
+					break;
+					default: delete eventObj[eventId];
+				}
+			});
+		
+		return this;
+	};
+
+	//element, types
+	Edom.prototype.trigger = function() {
+		var el = _isElement(arguments[0]) ? arguments[0] : this.element,
+			types = _isElement(arguments[0]) ? arguments[1] : arguments[0],
+			args = Array.prototype.slice.call(arguments, 1) || [],
+			eventName, eventId;
+
+		if (!el) return this;
+
+		eventsIterator(el, types, function(name, id, arr, ifCounter) {
+			arr.forEach(function(eventHandler) {
+				eventHandler.apply(null, args);
+			});
+		});
+
+		return this;
+	};
+
+	window.Edom = Edom;
+	window.Edom.e = new Edom();
+}(this));
---
+++ README.md
@@ -0,0 +1,86 @@
+#DOM events
+
+Edom - global constructor;
+
+##Bind events, ver.1
+```javascript
+var e = new Edom(),
+	element = document.getElementById('test');
+	handler = function(e) {
+		console.log('Click');
+		console.log(e.data); //{test : 'test'}
+	};
+
+//Simple bind
+e.on(element, 'click', handler);
+e.on(element, 'click', {test : 'test'}, handler);
+
+//Bind with event ID
+e.on(element, 'click.someID', handler);
+
+//Custom event
+e.on(element, 'myEvent', handler);
+e.on(element, 'myEvent.myID', handler);
+```
+
+##Bind events, ver.2
+```javascript
+var e = new Edom( document.getElementById('test') ),
+	handler = function() {
+		alert('Click');
+	};
+
+//Simple bind
+e.on('click', handler);
+
+//Bind with event ID
+e.on('click.someID', handler);
+
+//Custom event
+e.on('myEvent', handler);
+e.on('myEvent.myID', handler);
+```
+
+##Unbind events
+```javascript
+var e = new Edom( document.getElementById('test') ),
+	handler = function() {
+		alert('Click');
+	};
+
+//Simple unbind
+e.on('click', handler);
+e.off('click');
+e.off('click', handler);
+
+//Unbind by event name and event ID or simple all events by event ID
+e.on('click.someID', handler);
+e.off('click.someID');
+e.off('.someID');
+
+//Custom event
+e.on('myEvent.myID', handler);
+e.off('myEvent', handler);
+e.off('myEvent');
+e.off('.myID');
+```
+
+##Trigger events
+```javascript
+var e = new Edom( document.getElementById('test') ),
+	handler = function() {
+		console.log(arguments);
+	};
+
+
+e.on('click.clickID', handler);
+
+e.trigger('click');
+e.trigger('.clickID');
+e.trigger('click', 1, 2, 3, 4, 5, 6);
+```
+
+- instance - __new Edom([DOM element])__
+- on - __Edom.e.on([element], events, [data] callback)__ bind events
+- off - __Edom.e.off([element], events, [callback])__ unbind events
+- trigger - __Edom.e.off([element], events, [data])__ trigger events
